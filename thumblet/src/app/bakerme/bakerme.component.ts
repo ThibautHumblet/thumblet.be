2f29484248b4685c8916c537448f1408d62833f8
Thibaut Humblet <thibaut.humblet@telenet.be>
2020-09-07 22:21:13 +0200

    Added bakerme content

--- thumblet/src/app/bakerme/bakerme.component.ts
+++ thumblet/src/app/bakerme/bakerme.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-bakerme',
   templateUrl: './bakerme.component.html',
-  styleUrls: ['./bakerme.component.css']
+  styleUrls: ['./bakerme.component.css', '../portfoliostyles.css']
 })
 export class BakermeComponent implements OnInit {
 
