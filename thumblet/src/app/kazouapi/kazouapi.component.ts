7b38b51956e555e341f4b3cf4aa072e9b975e845
Thibaut Humblet <thibaut.humblet@telenet.be>
2020-11-04 01:45:35 +0100

    Added Kazou API content

--- thumblet/src/app/kazouapi/kazouapi.component.ts
+++ thumblet/src/app/kazouapi/kazouapi.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-kazouapi',
   templateUrl: './kazouapi.component.html',
-  styleUrls: ['./kazouapi.component.css']
+  styleUrls: ['./kazouapi.component.css', '../portfoliostyles.css']
 })
 export class KazouapiComponent implements OnInit {
 
