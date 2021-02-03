a1a13c8c1dadaefb077ae15e3b0e86b55ee41cc2
Thibaut Humblet <thibaut.humblet@telenet.be>
2021-02-03 15:00:13 +0100

    Added HAP content

--- thumblet/src/app/hap/hap.component.ts
+++ thumblet/src/app/hap/hap.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-hap',
   templateUrl: './hap.component.html',
-  styleUrls: ['./hap.component.css']
+  styleUrls: ['./hap.component.css',
+  '../portfoliostyles.css']
 })
 export class HapComponent implements OnInit {
 
