8d85ce08e6d298800dcb03d93d98da1afdbb527e
Thibaut Humblet <thibaut.humblet@telenet.be>
2021-02-19 00:45:25 +0100

    Added Kanari to projects

--- thumblet/src/app/kanari/kanari.component.ts
+++ thumblet/src/app/kanari/kanari.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-kanari',
   templateUrl: './kanari.component.html',
-  styleUrls: ['./kanari.component.css']
+  styleUrls: ['./kanari.component.css',
+  '../portfoliostyles.css']
 })
 export class KanariComponent implements OnInit {
 
