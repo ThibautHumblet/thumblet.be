89e75148425fbe2adab487d743290096d58d4f97
Thibaut Humblet <thibaut.humblet@telenet.be>
2021-06-24 15:09:13 +0200

    WIP matching content

--- thumblet/src/app/matching-and-sourcing/matching-and-sourcing.component.ts
+++ thumblet/src/app/matching-and-sourcing/matching-and-sourcing.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-matching-and-sourcing',
   templateUrl: './matching-and-sourcing.component.html',
-  styleUrls: ['./matching-and-sourcing.component.css']
+  styleUrls: ['./matching-and-sourcing.component.css',
+  '../portfoliostyles.css']
 })
 export class MatchingAndSourcingComponent implements OnInit {
 
